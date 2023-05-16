d510a251c4342c43a67f1e2a655d324de8c77f27
gabriela035 <szuiogangabriela@gmail.com>
2023-05-16 15:02:42 +0300

    changed profile_page

--- lib/features/presentation/page/profile/profile_page.dart
+++ lib/features/presentation/page/profile/profile_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/features/presentation/page/credential/sign_in_page.dart';
 import 'package:instagram_clone/features/presentation/page/profile/theme.dart';
+import 'BottomModalSheet.dart';
 
 import '../../../../consts.dart';
 import 'edit_profile_page.dart';
@@ -135,111 +136,6 @@ class ProfilePage extends StatelessWidget {
   }
 
   _openBottomModalSheet(BuildContext context) {
-    return showModalBottomSheet(
-        context: context,
-        builder: (context) {
-          return Container(
-            height: 190,
-            decoration: BoxDecoration(color: backGroundColor.withOpacity(.8)),
-            child: SingleChildScrollView(
-              child: Container(
-                margin: EdgeInsets.symmetric(vertical: 10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 10.0),
-                      child: Text(
-                        "More Options",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                            color: primaryColor),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 8,
-                    ),
-                    Divider(
-                      thickness: 1,
-                      color: secondaryColor,
-                    ),
-                    SizedBox(
-                      height: 8,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 10.0),
-                      child: GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => EditProfilePage()));
-                        },
-                        child: Text(
-                          "Edit Profile",
-                          style: TextStyle(
-                              fontWeight: FontWeight.w500,
-                              fontSize: 16,
-                              color: primaryColor),
-                        ),
-                      ),
-                    ),
-                    sizeVer(7),
-                    //theme
-                    Divider(
-                      thickness: 1,
-                      color: secondaryColor,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 10.0),
-                      child: GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => ThemeEdit()));
-                        },
-                        child: Text(
-                          "Edit Theme",
-                          style: TextStyle(
-                              fontWeight: FontWeight.w500,
-                              fontSize: 16,
-                              color: primaryColor),
-                        ),
-                      ),
-                    ),
-                    sizeVer(7),
-                    Divider(
-                      thickness: 1,
-                      color: secondaryColor,
-                    ),
-                    sizeVer(7),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 10.0),
-                      child: GestureDetector(
-                        //
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => SignInPage()));
-                        }, //
-                        child: Text(
-                          "Logout",
-                          style: TextStyle(
-                              fontWeight: FontWeight.w500,
-                              fontSize: 16,
-                              color: primaryColor),
-                        ),
-                      ), //
-                    ),
-                    sizeVer(7),
-                  ],
-                ),
-              ),
-            ),
-          );
-        });
+    BottomModalSheet.open(context);
   }
 }
